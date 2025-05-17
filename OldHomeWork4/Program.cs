d2f99c0089edddecb9276d34e6216ce9959b6ec1
Kyrylo <beatxprod@gmail.com>
2025-05-17 17:58:10 +0300

    add method:Task4()

--- OldHomeWork4/Program.cs
+++ OldHomeWork4/Program.cs
@@ -6,6 +6,26 @@ namespace OldHomeWork4
     {
         public static void Main(string[] args)
         {
+            Task4();
+        }
+
+        private static void Task4()
+        {
+            try
+            {
+                char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+                for (int i = 0; i < alphabet.Length; i++)
+                {
+                    ResultMessage($"{alphabet[i]} -> {(byte)alphabet[i]}");
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                ResultMessage($"Error: {e.Message}");
+            }
         }
 
         private static void ResultMessage(string message)
