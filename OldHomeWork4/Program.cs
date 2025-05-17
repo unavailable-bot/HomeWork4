cd9363daadea4cd9623d9c8cae3aa6ecbdaecd29
Kyrylo <beatxprod@gmail.com>
2025-05-17 16:57:26 +0300

    add method:Task1()

--- OldHomeWork4/Program.cs
+++ OldHomeWork4/Program.cs
@@ -6,6 +6,34 @@ namespace OldHomeWork4
     {
         public static void Main(string[] args)
         {
+            Task1();
+        }
+
+        private static void Task1()
+        {
+            try
+            {
+                int[] array = new int[50];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        array[i] += 2;
+                        continue;
+                    }
+                    array[i] += array[i - 1] + 2;
+                }
+
+                foreach (var value in array)
+                {
+                    ResultMessage($"Element: {value}");
+                }
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                ResultMessage($"Error: {e.Message}");
+            }
         }
 
         private static void ResultMessage(string message)
