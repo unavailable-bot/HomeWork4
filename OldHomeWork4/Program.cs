62e3b7296cd793ee96f358df4e937edb99419f95
Kyrylo <beatxprod@gmail.com>
2025-05-17 17:50:39 +0300

    create method:Task3dot1() & add into method:Task3()

--- OldHomeWork4/Program.cs
+++ OldHomeWork4/Program.cs
@@ -16,11 +16,29 @@ namespace OldHomeWork4
             {
                 string inputString = Console.ReadLine();
                 ResultMessage($"Count of chars: {inputString?.Length}");
+                
+                Task3dot1(inputString);
+                Console.ReadKey();
             }
             catch (Exception e)
             {
                 ResultMessage($"Error: {e.Message}");
             }
+        }
+
+        private static void Task3dot1(string inputString)
+        {
+            Console.WriteLine("Which symbol do you want to find?");
+            ConsoleKeyInfo inputSymbol = Console.ReadKey();
+            int inputSymbolCount = 0;
+            foreach (var character in inputString)
+            {
+                if (char.ToLower(inputSymbol.KeyChar) == character || char.ToUpper(inputSymbol.KeyChar) == character)
+                {
+                    inputSymbolCount++;
+                }
+            }
+            ResultMessage($"\nCount of {inputSymbol.KeyChar} is {inputSymbolCount}");
         }
 
         private static void ResultMessage(string message)
