65ba64ba6a2d9011437f47468288f1a03ddc95ce
Kyrylo <beatxprod@gmail.com>
2025-05-17 16:50:21 +0300

    start point:add method:ResultMessage()

---
+++ OldHomeWork4/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace OldHomeWork4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        private static void ResultMessage(string message)
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine(message);
+        }
+    }
+}
