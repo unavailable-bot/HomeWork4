0d4060448e9143c757805722d1e07529ea4f6b81
Kyrylo <beatxprod@gmail.com>
2025-05-17 17:32:05 +0300

    add method:Task2()

--- OldHomeWork4/Program.cs
+++ OldHomeWork4/Program.cs
@@ -4,8 +4,32 @@ namespace OldHomeWork4
 {
     internal class Program
     {
+        private static Random _rand = new Random();
+        private static string result = "It's ";
+        private static int evenNumbers;
         public static void Main(string[] args)
         {
+            try
+            {
+                int[] array = new int[_rand.Next(24, 51)];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = _rand.Next(0, int.MaxValue);
+                    if (array[i] % 2 == 0)
+                    {
+                        evenNumbers++;
+                        result += $"{array[i]}, ";
+                    }
+                }
+                
+                ResultMessage($"Even numbers count: {evenNumbers}\n{result}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                ResultMessage($"Error: {e.Message}");
+            }
         }
 
         private static void ResultMessage(string message)
