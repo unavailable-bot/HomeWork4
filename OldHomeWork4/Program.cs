70ace3e77bdefa47ac09d24d9c80c4d47d8c8fc8
Kyrylo <beatxprod@gmail.com>
2025-05-17 17:37:34 +0300

    add method:Task3()

--- OldHomeWork4/Program.cs
+++ OldHomeWork4/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 
 namespace OldHomeWork4
 {
@@ -6,6 +7,20 @@ namespace OldHomeWork4
     {
         public static void Main(string[] args)
         {
+            Task3();
+        }
+
+        private static void Task3()
+        {
+            try
+            {
+                string inputString = Console.ReadLine();
+                ResultMessage($"Count of chars: {inputString?.Length}");
+            }
+            catch (Exception e)
+            {
+                ResultMessage($"Error: {e.Message}");
+            }
         }
 
         private static void ResultMessage(string message)
